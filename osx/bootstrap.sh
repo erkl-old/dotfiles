441abb23d858e35b021fe3275512c379eafd2b75
Erik Lundin <erik@wub.io>
2013-03-27 14:46:12 +0000

    osx: add QLStephen

--- osx/bootstrap.sh
+++ osx/bootstrap.sh
@@ -8,5 +8,10 @@ sudo mkdir -p "/usr/local"
 sudo chown root:wheel "/usr/local"
 sudo chmod ug=rwx,o=rx "/usr/local"
 
+# install http://whomwah.github.com/qlstephen/
+mkdir -p "$HOME/Library/QuickLook"
+tar -C "$HOME/Library/QuickLook" -xzf "$DOTFILES/osx/QLStephen.tar.gz"
+open "$HOME/Library/QuickLook/QLStephen.qlgenerator"
+
 # update GUI preferences
 sh $DOTFILES/osx/preferences.sh
