fc521b0cd2679629be320b47fbe61212c15a735b
Erik Lundin <erik@wub.io>
2013-03-27 11:36:51 +0000

    osx: add bootstrap.sh

---
+++ osx/bootstrap.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+# add the current user to wheel
+sudo dscl . -append "/Groups/wheel" GroupMembership `whoami`
+
+# make sure /usr/local exists, with the correct permissions
+sudo mkdir -p "/usr/local"
+sudo chown root:wheel "/usr/local"
+sudo chmod ug=rwx,o=rx "/usr/local"
